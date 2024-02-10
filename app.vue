91de33fb154a404d63f58f1682730411dbb8c3de
JaeWoong Suh <90325840+SuhJae@users.noreply.github.com>
2024-02-10 18:55:21 +0900

    feat:nav bar

--- app.vue
+++ app.vue
@@ -1,52 +1,7 @@
-<script setup lang="ts">
-const links = [
-  {
-    label: "Home",
-    icon: "i-heroicons-book-open",
-    to: "/",
-  },
-];
-
-const { locale, locales } = useI18n();
-
-const items = [
-  [
-    {
-      label: "English",
-    },
-  ],
-];
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <UHeader :links="links">
-    <template #logo>
-      <img src="~/assets/logo.png" class="h-7 w-7" alt="Joseon Space Logo" />
-    </template>
-
-    <template #right>
-      <UDocsSearchButton label="" />
-
-      <UColorModeButton />
-
-      <UButton
-        to="https://github.com/nuxt/ui"
-        target="_blank"
-        icon="i-simple-icons-github"
-        color="gray"
-        variant="ghost"
-      />
-
-      <!-- Language dropdown -->
-      <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-        <UButton
-          color="white"
-          label="Options"
-          trailing-icon="i-heroicons-chevron-down-20-solid"
-        />
-      </UDropdown>
-    </template>
-  </UHeader>
+  <Navbar />
 
   <UMain>
     <NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,24 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/ui", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/i18n",
+    "@nuxt/content",
+    "@nuxtjs/fontaine",
+    "@nuxtjs/google-fonts",
+  ],
 
-  ui: {
-    global: true,
+  // Fonts
+  fontMetrics: {
+    fonts: ["DM Sans"],
+  },
+  googleFonts: {
+    display: "swap",
+    download: true,
+    families: {
+      "DM+Sans": [400, 500, 600, 700],
+    },
   },
 
   i18n: { vueI18n: "./i18n.config.ts" },
